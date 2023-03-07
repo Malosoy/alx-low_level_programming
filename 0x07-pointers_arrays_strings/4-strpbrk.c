e80fdd2d26b3a4603ecdeee105c30546ec2be541
Malosoy <faithbar116@gmail.com>
2023-03-07 12:16:56 -0800

    Locate firs occurrence of a set of any bytes of a string in a string

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,10 +2,27 @@
 
 /**
  * _strpbrk - function that searches a string for any of a set of bytes
- * @s:
- * @accept:
- * Return:
+ * @s: input
+ * @accept: input
+ * Return: a pointer to the byte in s,
+ * that matches one of the bytes in accept
+ * if no such byte is found, NULL
  */
 
 char *_strpbrk(char *s, char *accept)
 {
+	int i;
+
+	while (*s)
+	{
+	for (i = 0; accept[i]; i++)
+	{
+		if (*s == accept[i])
+		{
+			return (s);
+		}
+	}
+	s++;
+	}
+	return (0);
+}
