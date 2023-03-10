d25bddddf062f10cef5a62027895d0f82d1ed3aa
Malosoy <faithbar116@gmail.com>
2023-03-10 08:51:26 -0800

    Print all arguments a program receives, including the first one

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,11 +2,20 @@
 
 /**
  * main - program that prints all arguments passed into it
- *
+ * including the first one
+ * @argc: argument count
+ * @argv: argument vector
  * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
-
+	int a;
+	
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+		(void)argc;
+	}
+	return (0);
 }
