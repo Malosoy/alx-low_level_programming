97a51f1f7052d88c19fbc52e3b0c2bb73a07ebab
Malosoy <faithbar116@gmail.com>
2023-03-10 06:50:15 -0800

    Create mandatory task files and complete task 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
